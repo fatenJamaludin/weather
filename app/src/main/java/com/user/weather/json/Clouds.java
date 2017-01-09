cb6b47c9186e3173367f103349de1c62a2691efc
Fatin Jamaludin <aten93.hzyra@gmail.com>
2017-01-09 13:45:48 +0800

    init

---
+++ app/src/main/java/com/user/weather/json/Clouds.java
@@ -0,0 +1,15 @@
+package com.user.weather.json;
+
+
+public class Clouds {
+
+    private String all;
+
+    public Clouds(String all) {
+        this.all = all;
+    }
+
+    public String getAll() {
+        return all;
+    }
+}
---
+++ app/src/main/java/com/user/weather/json/FiveDaysForecast.java
@@ -0,0 +1,14 @@
+package com.user.weather.json;
+
+public class FiveDaysForecast {
+
+    private Forecast dailyForecast;
+
+    public FiveDaysForecast(Forecast dailyForecast) {
+        this.dailyForecast = dailyForecast;
+    }
+
+    public Forecast getDailyForecast() {
+        return dailyForecast;
+    }
+}
